b3a758311ec8b562e7157411affd6f5edb422054
Carlos Henrique <carlosh.sales@outlook.com>
2024-06-18 20:56:45 -0300

    features

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core.Singleton;
+using DG.Tweening;
+
+public class GameManager : Singleton<GameManager>
+{
+    [Header( "Player" )]
+    public GameObject playerPrefab;
+
+    [Header( "Enemies" )]
+    public List<GameObject> enemies;
+
+    [Header( "References" )]
+    public Transform startPoint;
+
+    private GameObject _currentPlayer;
+
+    [Header( "Animation" )]
+    public float duration = .2f;
+    public float delay = .5f;
+    public Ease easy = Ease.OutBack;
+
+    private void Start ()
+    {
+        Init();
+    }
+
+    public void Init()
+    {
+        SpawnPlayer();
+    }
+
+
+    private void SpawnPlayer()
+    {
+        _currentPlayer = Instantiate(playerPrefab);
+        _currentPlayer.transform.position = startPoint.transform.position;
+        _currentPlayer.transform.DOScale(0, duration).SetEase( easy ).From();
+    }
+
+}
--- Assets/Scripts/Menu/MenuButtonsManeger.cs
+++ Assets/Scripts/Menu/MenuButtonsManeger.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class MenuButtonsManeger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<GameObject> buttons;
+
+    [Header( "Animation" )]
+    public float duration = .2f;
+    public float delay = .5f;
+
+    private void Awake ()
     {
-        
+        HideAllButtons();
+        ShowButtons();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HideAllButtons ()
     {
-        
+        foreach (var button in buttons)
+        {
+            button.transform.localScale = Vector3.zero;
+            button.SetActive( false );
+        }
     }
+
+    private void ShowButtons()
+    {
+        foreach (var button in buttons)
+        {
+            button.SetActive(true);
+            button.transform.DOScale(1, duration).SetDelay(delay);
+        }
+    }
+
+
+    /*
+    IEnumerator ShowButtonsWithDelay ()
+    {
+        foreach (var button in buttons)
+        {
+            button.SetActive(true);
+            button.transform.DOScale(1, duration);
+            yield
+        }
+    }
+    */
 }
